11b8f771e3fb7d89691e4babac1996ccf1cfeb54
TySabs <tysabs@gmail.com>
2018-11-29 18:43:10 -0600

    Add pie chart data

--- UnchartedApp/PieChart.Designer.cs
+++ UnchartedApp/PieChart.Designer.cs
@@ -28,12 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea8 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend8 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.MainLabel = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.BackButton = new System.Windows.Forms.Button();
+            this.SourceLabel = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.panel1.Controls.Add(this.SourceLabel);
+            this.panel1.Controls.Add(this.BackButton);
+            this.panel1.Controls.Add(this.MainLabel);
+            this.panel1.Controls.Add(this.chart1);
+            this.panel1.Location = new System.Drawing.Point(293, 33);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1638, 1327);
+            this.panel1.TabIndex = 0;
+            // 
+            // MainLabel
+            // 
+            this.MainLabel.AutoSize = true;
+            this.MainLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.MainLabel.Location = new System.Drawing.Point(12, 26);
+            this.MainLabel.Name = "MainLabel";
+            this.MainLabel.Size = new System.Drawing.Size(1546, 82);
+            this.MainLabel.TabIndex = 1;
+            this.MainLabel.Text = "Most Popular Frameworks, Libraries, and Tools";
+            // 
+            // chart1
+            // 
+            chartArea8.AxisY.Title = "Popularity";
+            chartArea8.Name = "MainArea";
+            this.chart1.ChartAreas.Add(chartArea8);
+            legend8.Name = "MainLegend";
+            legend8.Title = "Framework";
+            this.chart1.Legends.Add(legend8);
+            this.chart1.Location = new System.Drawing.Point(44, 141);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(1551, 949);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // BackButton
+            // 
+            this.BackButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.BackButton.Location = new System.Drawing.Point(603, 1175);
+            this.BackButton.Name = "BackButton";
+            this.BackButton.Size = new System.Drawing.Size(400, 125);
+            this.BackButton.TabIndex = 4;
+            this.BackButton.Text = "Back to Main Menu";
+            this.BackButton.UseVisualStyleBackColor = true;
+            this.BackButton.Click += new System.EventHandler(this.BackButton_Click);
+            // 
+            // SourceLabel
+            // 
+            this.SourceLabel.AutoSize = true;
+            this.SourceLabel.Location = new System.Drawing.Point(39, 1102);
+            this.SourceLabel.Name = "SourceLabel";
+            this.SourceLabel.Size = new System.Drawing.Size(716, 29);
+            this.SourceLabel.TabIndex = 5;
+            this.SourceLabel.Text = "Source: StackOverflow Developer Survey 2018 -- All Respondents";
+            // 
+            // PieChart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(2273, 1372);
+            this.Controls.Add(this.panel1);
+            this.Name = "PieChart";
             this.Text = "PieChart";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label MainLabel;
+        private System.Windows.Forms.Button BackButton;
+        private System.Windows.Forms.Label SourceLabel;
     }
 }
--- UnchartedApp/PieChart.cs
+++ UnchartedApp/PieChart.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace UnchartedApp
 {
@@ -17,6 +19,46 @@ namespace UnchartedApp
             InitializeComponent();
             Size = Program.InitFrameSize();
             CenterToScreen();
+            InitDataPoints();
+        }
+
+
+        private void InitDataPoints()
+        {
+            Series newSeries = new Series();
+
+            newSeries.ChartArea = "MainArea";
+            newSeries.ChartType = SeriesChartType.Pie;
+            newSeries.IsValueShownAsLabel = true;
+            newSeries.Name = "Popularity";
+
+            string dataSrc = "../../Properties/Resources/popular-frameworks.txt";
+
+            using (StreamReader inFile = new StreamReader(dataSrc))
+            {
+                string buffer;
+                while ((buffer = inFile.ReadLine()) != null)
+                {
+                    string[] splitLine = buffer.Split('#');
+
+                    double newValue = Double.Parse(splitLine[0]);
+
+                    DataPoint newPoint = new DataPoint();
+                    newPoint.SetValueY(newValue);
+                    newPoint.AxisLabel = splitLine[1];
+
+                    newPoint.LabelFormat = "#.0\'%\'";
+
+                    newSeries.Points.Add(newPoint);
+                }
+            }
+
+            chart1.Series.Add(newSeries);
+        }
+
+        private void BackButton_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
